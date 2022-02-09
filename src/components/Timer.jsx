2205637fb52e118afba19d404a643be12296fd5e
tessoka <akos.gollitz@gmail.com>
2022-02-10 00:24:27 +0100

    count down works roughly

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,22 +3,45 @@ import React, { useEffect, useState } from 'react'
 const Timer = () => {
 
   const [ isRunning, setIsRunning ] = useState(false)
-  const [ workTime, setWorkTime ] = useState(1500)
+  const [ timeLeft, setTimeLeft ] = useState(130)
+  const [ displayTime, setDisplayTime ] = useState({})
 
   const handleClickStart = () => {
+    if (!isRunning) setTimeLeft(timeLeft - 1)
     setIsRunning(!isRunning)
     console.log("clicked")
   }
 
+  const calculateDisplayTime = () => {
+    let mins = Math.floor(timeLeft / 60)
+    let secs = timeLeft - (mins * 60)
+    if (mins < 10) mins = "0" + mins.toString()
+    if (secs < 10) secs = "0" + secs.toString()
+    setDisplayTime({mins, secs})
+  }
+
   useEffect(() => {
-    console.log("haha")
-  }, [])
+    if (isRunning && timeLeft !== 0) {
+      setTimeout(() => {
+        setTimeLeft(timeLeft-1)
+      }, 1000)
+    } else {
+      console.log("end of count down")
+      setIsRunning(false)
+    }
+
+    calculateDisplayTime()
+    console.log(displayTime)
+
+  }, [timeLeft])
 
   return (
     <div className="container-timer">
       <div className="timer-progressbar">
         <div className="timer">
-          25:00
+          <div className="timer-mins">{displayTime.mins}</div>
+          <div className="timer-colon">&#58;</div>
+          <div className="timer-secs">{displayTime.secs}</div>
         </div>
         <div className="timer-type">
           Work
